2e30b6995595827fec67f07b40d87ab34a1b1ec5
JOSE MARCOS LUNA VALDEZ <jlunav@ad.elektra.com.mx>
2018-03-22 09:33:13 -0600

    customize Jenkins Image

--- build_run.sh
+++ build_run.sh
@@ -11,6 +11,10 @@ docker network create -d bridge --subnet 10.51.145.0/23 --gateway 10.51.144.1 do
 
 echo "Building Giblit Server"
 docker build -t gitblit gitblit/
+
+echo "Building Jenkins Server"
+docker build --rm -t swfactory/jenkins:lts jenkins/
+
 docker-compose up 
 
 # docker-compose down
