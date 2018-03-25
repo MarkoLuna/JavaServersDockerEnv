b26b5960dd0fa4dc5ab26c3704ec6c1a50c9d718
Marko <Marko@DESKTOP-1SVUTQP>
2018-03-24 21:48:31 -0600

    remove network

--- build_run.sh
+++ build_run.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$(docker network ls | grep dockernet)" ]; then
-    echo "Removing dockernet ..." 
-    echo 
-    docker network rm dockernet
-fi
-
-echo "Creating network ..."
-docker network create -d bridge --subnet 10.51.145.0/23 --gateway 10.51.144.1 dockernet
-
 echo "Building Giblit Server"
 docker build -t gitblit gitblit/
 
 echo "Building Jenkins Server"
-docker build --rm -t swfactory/jenkins:lts jenkins/
+docker build -t swfactory/jenkins:lts jenkins/
 
 docker-compose up 
 
